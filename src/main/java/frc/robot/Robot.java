3eeddf7405c6bdc26a0078c0ed588e7e4f7420c7
Levi Yates <94545949+Bizaare@users.noreply.github.com>
2024-04-03 20:26:35 -0500

    Save for field

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -50,6 +50,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
+    m_robotContainer.getDrivetrain().Pigeon2Reset();
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     if (m_autonomousCommand != null) {
